2df8d86e0498ccd64039ae632c6edad06615a3ac
Andrés <c.garcia@diatomea.tech>
2019-10-21 12:44:08 +0200

    structure advances and understading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cheerio = require('cheerio');
 const wget = require('wget-improved');
 let fs = require('fs');
+const _ = require('lodash')
 
 
 const source = 'http://0.0.0.0:1234/test.html';
@@ -8,11 +9,8 @@ outputFile = './output/archive.html';
 
 ///helper functions 
 
-const fetchData = (callback) => {
-    let download = wget.download(source, outputFile);
-    return download.on('end', () => {
-        return callback();
-    });
+const fetchData = () => {
+    wget.download(source, outputFile);
 };
 
 const readData = () => {
@@ -24,17 +22,103 @@ const readData = () => {
 const constructTree = async () => {
     let structure = {};
 
-    await fetchData(readData)
-    let string = readData();
+    await fetchData()
+    let string = await readData();
     // console.log("html string for cherio", string);
     const $ = cheerio.load(string)
     $.html();
 
-    console.log($('#superTitle').text());
-    console.log($.root().html());
+    //get all 
+    let input = $('#algo');
+    //console.log(input);   
+    //console.log($('li').next());
+    //console.log($.root().html());
 
+    let allElem = $('*');
+
+    //console.log("thttt", allElem[7].children)
+
+    _.forEach(allElem[7].children, (es)=>{
+        console.log("im ", es.type)
+    })
+
+    console.log("recursive", recursiveChildCheck(allElem[2].children))
+
+    _.forEach(allElem, (e, i)=>{
+        console.log("im index ", i, "and im",e.name, "and i have", e.children.length, "children")
+        let children = {};
+        // e.children.forEach(child=>{
+
+            //recursive function
+            //break point:
+            //if childs are not tag type.
+            // let flag = true;
+
+            // children.forEach(child => {
+            //     if(_.isEqual(child.type, "tag")){
+            //         flag = false;
+            //     }
+            // });
+
+
+
+            // if(_.isEqual(child.type, "tag")){
+            //     let type = child.type;
+            //     children={
+            //         ...children,
+            //         [type] : e.children[0].data || "null"
+            //     }   
+            // }
+
+        // })
+
+        let type = e.name;
+        structure.body= {
+            ...structure.body,
+            [type] : e.children[0].data || "null"
+
+        };
+
+    })
+    console.log(structure);
+
+  
 
 };
+
+const recursiveChildCheck = (childs, structure) => {
+
+    // having {
+    //     body: {}
+    // }
+
+    let children = childs;
+    let mycurrentS = structure;
+
+    let currentTags = children.filter(e=> _.isEqual(e.type, "tag"))
+
+    //base case (no more tags inside, just text)
+    if (currentTags.length == 0){
+        //chldren has text elements 
+
+        //pass in the structure ?
+            return {
+                value: children[0].data || "null",
+                attributes : children[0].att || "null",
+                id: children[0].type || "NA",
+                class: "NA"
+             }
+    }
+   
+    if(currentTags.length > 0){
+
+        return (children, {
+            // new structure. structure 
+        })
+
+    }
+}
+
 
 
 constructTree();
