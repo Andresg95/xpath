69f0f3c4ceeb2720f7a9e4eba4b3bab97983af66
Andrés <c.garcia@diatomea.tech>
2019-10-23 15:07:49 +0200

    # xpath structure functional

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let fs = require('fs');
 const _ = require('lodash')
 
 
-const source = 'http://0.0.0.0:1234/test.html';
+const source = 'http://0.0.0.0:1234/test1.html';
 outputFile = './output/archive.html';
+let finalNodesXpaths = [];
+
 
 ///helper functions 
 
@@ -15,62 +17,7 @@ const fetchData = () => {
 
 const readData = () => {
     return data = fs.readFileSync(outputFile, 'utf8').toString('utf8');
-}
-
-
-//create tree with ids and classes if available.
-const constructTree = async () => {
-    let structure = {};
-
-    await fetchData()
-    let string = await readData();
-    // console.log("html string for cherio", string);
-    const $ = cheerio.load(string)
-    $.html();
-
-
-    const html = $('*').toArray()[0];
-
-    let stringedHTML = (JSON.stringify(recursive(html)));
-    let trimmed = stringedHTML.replace(/\"\n"/g, '');
-
-    let final = JSON.parse(trimmed);
-
-    console.log(trimmed);
-    //console.log(final.html.values[4].body.values[1].h1.values[0].value);
-    
-
-    //get all 
-    let allElem = $('*');
-    let input = $('#great');
-
-    console.log({input});
-    console.log("now this", allElem[7].attribs)
-
-
-
-    /*
-    _.forEach(allElem[0].children, (es)=>{
-        console.log("im ", es.type)
-    })
-
-
-    _.forEach(allElem, (e, i)=>{
-        console.log("im index ", i, "and im",e.name, "and i have", e.children.length, "children");
-
-        let type = e.name;
-        structure.body= {
-            ...structure.body,
-            [type] : e.children[0].data || "null"
-
-        };
-
-    })
-   
-*/
 };
-
-constructTree();
 
 
 /**
@@ -85,32 +32,78 @@ constructTree();
  * }
  */
 
-const recursive=(obj)=>{
 
+const recursive = (obj,parent) => {
 
-    if(obj.type=="tag"){
-
+    if (obj.type == "tag") {
+        //console.log(parent)
+        let currentParent = (!_.isEqual(obj.name, "html")) ? `${parent}.${obj.name}` : obj.name;
         const o = {
-            [obj.name] : {
+            [obj.name]: {
                 class: obj.attribs.class || "NA",
                 id: obj.attribs.id || "NA",
                 attributes: obj.attribs || {},
-                values:obj.children.map(child=>{       
-                    return recursive(child)
+                xpath: currentParent,
+                values: obj.children.map((child,index) => {
+                    return recursive(child,`${currentParent}.values[${index}]`)
                 })
             }
         }
         delete o[obj.name].attributes.id;
         delete o[obj.name].attributes.class;
-        
+
         return o;
     }
-    if(obj.type=="text"){
+    if (obj.type == "text") {
         //base case
-        return obj.data != "\n" ? {value: obj.data} : obj.data
+        finalNodesXpaths.push({
+            value: obj.data,
+            xpath :`${parent}.value`})
+        
+        return obj.data != "\n" ? { value: obj.data } : obj.data
     }
 
 }
 
 
+//create tree with ids and classes if available.
+const constructTree = async () => {
+
+    await fetchData()
+    let string = await readData();
+    // console.log("html string for cherio", string);
+    const $ = cheerio.load(string)
+    $.html();
+    const html = $('*').toArray()[0];
+
+    return (recursive(html,"", []));
+};
+
+
+
+const matchingRegularExp = (number) => {
+
+    return new RegExp(number.toString(),"gm");
+};
+
+const getXpathsforValue = async (val) => {
+    //main function.
+    let html = await constructTree();
+    let re = matchingRegularExp(val);
+
+       
+    results = finalNodesXpaths.filter((e)=>e.value.match(re))
+    console.log(results);
+
+    //html.values[4].body.values[1].h1.values[0].value
+    //concat varname. + xpath
+    
+
+}
+
+
+
+
+
+getXpathsforValue(2.43);
 
