ddc2aa990bb4d89527f20b5c7445eb4facc5161c
Andrés <c.garcia@diatomea.tech>
2019-10-28 13:02:53 +0100

    avances algoritmo identificador de nodos

--- index.js
+++ index.js
@@ -7,14 +7,12 @@ const _ = require('lodash')
 
 const source = "http://0.0.0.0:1234/test1.html";
 outputFile = './output/archive.html';
-let finalNodesXpaths = [];
 
 
 ///helper functions 
-const fetchData = async () => {
+const fetchData = async (source) => {
 
     let result;
-
     await axios(
         source
     ).then(response => {
@@ -28,65 +26,58 @@ const fetchData = async () => {
     return await result;
 };
 
+
 const readData = () => {
     return data = fs.readFileSync(outputFile, 'utf8').toString('utf8');
 };
 
 
-/**
- * 
- * obj {
- *  type: string
- *  children: obj[]
- * }
- * @param {*} obj 
- * {
- *   obj
- * }
- */
-
-
-const recursive = (obj, parent) => {
-
-    if (obj.type == "tag") {
-        //console.log(parent)
-        let currentParent = (!_.isEqual(obj.name, "html")) ? `${parent}.${obj.name}` : obj.name;
-        const o = {
-            [obj.name]: {
-                class: obj.attribs.class || "NA",
-                id: obj.attribs.id || "NA",
-                attributes: obj.attribs || {},
-                xpath: currentParent,
-                values: obj.children.map((child, index) => {
-                    return recursive(child, `${currentParent}.values[${index}]`)
-                })
-            }
-        }
-        delete o[obj.name].attributes.id;
-        delete o[obj.name].attributes.class;
-
-        return o;
-    }
-    if (obj.type == "text") {
-        //base case
-        finalNodesXpaths.push({
-            value: obj.data,
-            xpath: `${parent}.value`
-        })
-
-        return obj.data != "\n" ? { value: obj.data } : obj.data
-    }
-}
-
-
 //create tree with ids and classes if available.
-const constructTree = async () => {
+const constructTree = async (source) => {
 
-    fs.writeFileSync(outputFile, await fetchData());
+    let finalNodesXpaths = [];
+
+    fs.writeFileSync(outputFile, await fetchData(source));
     const $ = cheerio.load(await readData())
     $.html();
     const html = $('*').toArray()[0];
-    return (recursive(html, "", []));
+
+    const recursive = (obj, parent, base) => {
+
+        if (obj.type == "tag") {
+            //console.log(parent)
+            let currentParent = (!_.isEqual(obj.name, "html")) ? `${parent}.${obj.name}` : obj.name;
+            const o = {
+                [obj.name]: {
+                    class: obj.attribs.class || "NA",
+                    id: obj.attribs.id || "NA",
+                    attributes: obj.attribs || {},
+                    xpath: currentParent,
+                    values: obj.children.map((child, index) => {
+                        return recursive(child, `${currentParent}.values[${index}]`, currentParent)
+                    })
+                }
+            }
+            delete o[obj.name].attributes.id;
+            delete o[obj.name].attributes.class;
+
+            return o;
+        }
+        if (obj.type == "text") {
+            //base case
+            finalNodesXpaths.push({
+                value: obj.data,
+                xpath: `${parent}.value`,
+                class: `${base}.class`,
+                attributes: `${base}.attributes`,
+                id: `${base}.id`,
+            })
+
+            return obj.data != "\\n" ? { value: obj.data } : obj.data
+        }
+    }
+
+    return [(recursive(html, "", [])), finalNodesXpaths];
 };
 
 
@@ -100,20 +91,20 @@ const matchingRegularExp = (number) => {
     if (groups.length == 1) {
 
         let decimalSeparator = groups[0].charAt(groups[0].length - 1) != " " ? `\\${groups[0].charAt(groups[0].length - 1)}` : "\\s";
-        console.log({ decimalSeparator });
+       // console.log({ decimalSeparator });
         let numGroups = RegExp(decimalSeparator, "g");
         let decimalLength = number.toString().split(numGroups)[1].length
         let wholeRegex = `(\\+\\-)?(\\d{1,100}${decimalSeparator})(\\d{0,${decimalLength}})`
-        console.log({ wholeRegex })
+        //console.log({ wholeRegex })
         return new RegExp(wholeRegex, "gm")
 
     };
-    console.log({ groups });
+    //console.log({ groups });
     let lastGroup = groups[groups.length - 1]; //the last symbol used
     let n = `\\${lastGroup.charAt(lastGroup.length - 1)}`
     let firstGroup = groups[0];
     let common = firstGroup.charAt(firstGroup.length - 1) != " " ? `\\${firstGroup.charAt(firstGroup.length - 1)}` : "\\s"
-    console.log("the common separator is ", { common });
+    //console.log("the common separator is ", { common });
     //devuelve 2 grupos, los enteros y los decimales separados por el separator
     let numGroups = RegExp(n, "g");
     let decimalLength = number.toString().split(numGroups)[1].length
@@ -126,61 +117,129 @@ const matchingRegularExp = (number) => {
 };
 
 
-
-
-
 const getXpathsforValue = async (val) => {
     //main function.
-    let html = await constructTree();
-    let results = valuesCleanUps(matchingRegularExp(val))
+    let data = await constructTree();
+    let html = data[0];
+    let finalNodesXpaths = data[1];
+    let results = valuesCleanUps(matchingRegularExp(val), finalNodesXpaths)
 
-
+    console.log({html});
+    
     console.log({
         countResults: results.length
-
     },
         { results });
-    //console.log(html);
+
+    return {
+        html,
+        results
+    }
 };
 
 
-const valuesCleanUps = (re) => {
+const valuesCleanUps = (re, finalNodesXpaths) => {
+
 
     let results = finalNodesXpaths.filter((e) => e.value.match(re))
     return results.map(item => {
 
-        let value = item.value.replace(/[^\d\\.-\\+,\\']/g, '');
-        let { xpath } = item;
+        let value = item.value.replace(/[^\\d\\.-\\+,\\']/g, '');
+        let { xpath, attributes, id, class: Class } = item;
         return {
             value,
-            xpath
+            xpath,
+            attributes,
+            id,
+            Class
         }
     })
 };
 
 
 
-const recurrentExecution = (html, alias) => {
+
+const main = async () => {
 
 
-    //make sure to remove finalNoesXpath global var prop.
+    let { html,
+        results,
+    } = await getXpathsforValue("0,41");
+    //set up aarray with aliases [{xpath, alias}]
+    let myalias = results.map((res)=>{return {
+        alias: "Myvalue",
+        xpath: res.xpath,
+        id: res.id,
+        Class: res.Class,
+        attributes: res.attributes
+    }});
+    recurrentExecution(source, myalias, html)
+    
+}
+
+main();
+
+const recurrentExecution = async (source, results, html) => {
+    
+    //we have current html object, nodevalues with xpath, and results.
+    //take xpath, from results.
+
+
     let count = 0;
-    const intervalObject = setInterval(()=>{
+    const intervalObject = setInterval(async () => {
         count++;
         console.log(count, "seconds passed");
-        if(count%5==0){
-            console.log('exiting');
-            getXpathsforValue("0,41");
+        if (count % 5 == 0) {
+            //releer la pagina
+                let HTML =  await fetchData(source);
+                const $ = cheerio.load(HTML)
+                $.html();
+                const html = $('*').toArray()[0];
+                console.log("ngaa", {html})
+
+                //getbackrecursive
+                [(recursive(html, "", [])), finalNodesXpaths];
+                
+
+
+            let alias ="Myvalue";
+            let spxpath = results.filter((o)=>o.alias==alias)[0];
+        
+            let sec = {
+                value: _.get(html, results[0].xpath),
+                id: _.get(html, spxpath.id),
+                Class:  _.get(html, spxpath.Class),
+                attributes: _.get(html, spxpath.attributes)
+        
+            }
+        
+            console.log({sec});
+        
+
+
+
+
+
+
+            //values for first coincidence:
+            let second = {
+                value: _.get(HTML, results[0].xpath),
+                id: _.get(HTML, results[0].id),
+                class: _.get(HTML, results[0].Class),
+                attributes: _.get(HTML, results[0].attributes)
+            }
+
+            console.log("new page, same stored paths",{second});
+
         }
 
-        if (count==20){
+        if (count == 20) {
             clearInterval(intervalObject)
         }
 
     }, 1000)
 };
 
-recurrentExecution();
 
 
 
